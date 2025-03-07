82213fb85d869eb0b08d6cca25ff2a005c243c6d
skybleesed <155318425+skybleesed@users.noreply.github.com>
2025-03-07 21:56:53 +0700

    Update do_you_love_me.js

--- do_you_love_me.js
+++ do_you_love_me.js
@@ -1,26 +1,24 @@
 const questionContainer = document.querySelector(".question-container");
-const resultContainer = document.querySelector(".result-container.yes");
+const resultContainer = document.querySelector(".result-container"); // Sửa ở đây
 const gifResult = document.querySelector(".gif-result");
 const heartLoader = document.querySelector(".cssload-main");
 const yesBtn = document.querySelector(".js-yes-btn");
 const noBtn = document.querySelector(".js-no-btn");
 
-// /change the postion of no button
+// Thay đổi vị trí của nút No
 noBtn.addEventListener("mouseover", () => {
   const newX = Math.floor(Math.random() * questionContainer.offsetWidth);
-  const newY = Math.floor(Math.random() * questionContainer.offsetWidth);
-
+  const newY = Math.floor(Math.random() * questionContainer.offsetHeight);
   noBtn.style.left = `${newX}px`;
   noBtn.style.top = `${newY}px`;
 });
 
-// yes button functionality
-
+// Xử lý sự kiện khi nhấn nút Yes
 yesBtn.addEventListener("click", () => {
   questionContainer.style.display = "none";
   heartLoader.style.display = "inherit";
 
-  const timeoutId = setTimeout(() => {
+  setTimeout(() => {
     heartLoader.style.display = "none";
     resultContainer.style.display = "inherit";
     gifResult.play();
